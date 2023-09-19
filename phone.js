00fde5da19f8b8a5c7d755c915ab7a7f332ded59
“rakibul8226” <rakibul8226@gmail.com>
2023-09-20 03:33:07 +0600

    fetch & show data from API

--- phone.js
+++ phone.js
@@ -1,47 +1,47 @@
-// const loadPhone = async () =>{
-//     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-//     const data = await res.json();
-//     const phones = data.data;
-//     console.log(phones);
-//    displayPhones(phones);
-// }
+const loadPhone = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones);
+   displayPhones(phones);
+}
 
 
-// const displayPhones = phones => {
-//     // console.log(phones);
-//      // Step 1 : get the  div container
-//     const phoneContainer = document.getElementById('phone-container')
-//     phones.forEach(phone =>{
-//         // console.log(phone);
+const displayPhones = phones => {
+    // console.log(phones);
+     // Step 1 : get the  div container
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone =>{
+        // console.log(phone);
 
-//         // Step 2 : create a div
-//         const phoneCard = document.createElement('div');
-//         phoneCard.classList =`card p-4 bg-gray-100 shadow-xl m-6`;
-//          // Step 3 : set innerHtml
-//         phoneCard.innerHTML=`
+        // Step 2 : create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList =`card p-4 bg-gray-100 shadow-xl m-6`;
+         // Step 3 : set innerHtml
+        phoneCard.innerHTML=`
         
-//         <figure><img src= "${phone.image}" /></figure>
-//                 <div class="card-body">
-//                   <h2 class="card-title">${phone.phone_name}</h2>
-//                   <h3 class="card-title">${phone.brand}</h3>
-//                   <p>${phone.slug}</p>
-//                   <div class="card-actions justify-end">
-//                     <button class="btn btn-primary">Buy Now</button>
-//                   </div>
-//                 </div>
-//         `
-//         phoneContainer.appendChild(phoneCard)
-//          // Step 4 : append child 
-//     })
-// }
+        <figure><img src= "${phone.image}" /></figure>
+                <div class="card-body">
+                  <h2 class="card-title">${phone.phone_name}</h2>
+                  <h3 class="card-title">${phone.brand}</h3>
+                  <p>${phone.slug}</p>
+                  <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+        `
+        phoneContainer.appendChild(phoneCard)
+         // Step 4 : append child 
+    })
+}
 
 
-// const handleSearch = () =>{
-//     const searchField = document.getElementById('search-field');
-//     const searchText =  searchField.value;
-//     console.log(searchText);
-// }
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText =  searchField.value;
+    console.log(searchText);
+}
 
 
 
-// loadPhone();
+loadPhone();
